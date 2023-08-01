e4c94d8f3e0c47a4406c1cb7634e5e1f30cfc26b
small-entropy <113969501+small-entropy@users.noreply.github.com>
2023-08-02 01:34:31 +0500

    Добавил часть массовых операций для Store
    Добавил массовое редактирование и массовое удаление (перманентное)

--- stores/mongo/methods_delete.go
+++ stores/mongo/methods_delete.go
@@ -3,17 +3,22 @@ package mongo
 import (
 	error_constants "github.com/small-entropy/go-backbone/constants/error"
 	"github.com/small-entropy/go-backbone/datatypes/record"
+	"github.com/small-entropy/go-backbone/datatypes/recordset"
 	backbone_error "github.com/small-entropy/go-backbone/error"
 	"github.com/small-entropy/go-backbone/utils/convert"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
+// DeleteOne
 // Метод удаления записи из коллекции
 func (s *MongoStore[DATA]) DeleteOne(filter map[string]interface{}) (record.Record[primitive.ObjectID, DATA], error) {
 	var err error
 	var result record.Record[primitive.ObjectID, DATA]
+
 	filter_bson := convert.MapToBsonM(filter)
+
 	if result, err = s.FindOne(filter_bson); err == nil {
 		_, err = s.Storage.DeleteOne(*s.Context, filter_bson)
 	} else {
@@ -25,4 +30,32 @@ func (s *MongoStore[DATA]) DeleteOne(filter map[string]interface{}) (record.Reco
 		}
 	}
 	return result, err
+}
+
+// DeleteMany
+// Метод удаления списка записей из коллекции по фильтру
+func (s *MongoStore[DATA]) DeleteMany(filter map[string]interface{}) (recordset.RecordSet[primitive.ObjectID, DATA], error) {
+	var err error
+	var results recordset.RecordSet[primitive.ObjectID, DATA]
+	var records []record.Record[primitive.ObjectID, DATA]
+
+	filter_bson := convert.MapToBsonM(filter)
+
+	var cursor *mongo.Cursor
+	if cursor, err = s.Storage.Find(*s.Context, filter_bson); err == nil {
+		defer cursor.Close(*s.Context)
+		if err = cursor.All(*s.Context, &records); err == nil {
+			results.SetItems(records)
+			_, err = s.DeleteMany(filter_bson)
+		} else {
+			err = &backbone_error.StoreError{
+				Status:       error_constants.ERR_STORE_DECODE,
+				StorageName:  s.Storage.Name(),
+				DatabaseName: s.Storage.Database().Name(),
+				Err:          err,
+			}
+		}
+	}
+
+	return results, err
 }
